70eb73b041466953a3c31afe1378b3f1032cae92
oliviaosterlund <oliviaosterlund@nyu.edu>
2025-06-21 20:20:19 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,4 +9,30 @@ import dagshub
 import shap
 import pycaret
 
+token = st.secrets["DAGSHUB_TOKEN"]
+dagshub.auth.add_app_token(token=token)
+
+dagshub.init(repo_owner='oliviaosterlund', repo_name='finalprojectapp', mlflow=True)
+
+from ydata_profiling import ProfileReport
+from streamlit_pandas_profiling import st_profile_report
+
+from sklearn.preprocessing import LabelEncoder
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
+from sklearn.tree import DecisionTreeRegressor
+from sklearn.ensemble import RandomForestRegressor
+from xgboost import XGBRegressor
+from sklearn import metrics
+
+st.set_page_config(
+    page_title="New York Housing",
+    layout="centered",
+    page_icon="🍎",
+)
+
+df = pd.read_csv("NY-House-Dataset.csv")
+st.sidebar.title("Student Habits vs Student Performance")
+page = st.sidebar.selectbox("Select Page",["Introduction","Data Visualization", "Automated Report","Predictions", "Explainability", "Pycaret" "MLFlow Runs"])
+
 
